1d6606eae4c23bc2330943020dfe8a8f52fb1176
Saramarque <132975803+Saramarque@users.noreply.github.com>
2023-06-30 00:21:13 -0600

    Add files via upload
    le di color a las letras, y un poco de diseño al contenedor

--- App.vue
+++ App.vue
@@ -16,10 +16,10 @@ export default {
 
 </script>
 <template>
-  <div class="container">
-    <h1>Login</h1>
+  <div class="container text-primary">
+    <h1 class="titulo" >iniciar sesion </h1>
     <form @submit.prevent="login">
-      <div class="form-group">
+      <div class="form-group1">
         <label for="username">Username</label>
         <input type="text" id="username" v-model="username" class="form-control" required>
       </div>
@@ -34,8 +34,40 @@ export default {
 
 <style scoped>
 .container {
+  display: flex;
+  background-color: rgb(127, 133, 131);
   margin-top: 100px;
   text-align: center;
+height: 500px;
+  width: 500px;
+  align-items: center;
+  flex-direction: column;
+  justify-content:space-evenly;
+  position: absolute;
+  top: 38%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-bottom: 2px;
+}
+
+button{
+  position: absolute;
+  top: 87%;
+  left: 32%;
+  transform: translate(-50%, -50%);
+}
+.form-group1{
+  position: absolute;
+  top: 55%;
+  left: 32%;
+  transform: translate(-50%, -50%);
+
+}
+.titulo{
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
 </style>
